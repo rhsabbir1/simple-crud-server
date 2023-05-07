6d17cf9a3b9400393074c8bae89ac2a244c074ca
rhsabbir1 <rhsabbir090@gmai.com>
2023-05-07 21:30:20 +0600

    get data clint side

--- index.js
+++ index.js
@@ -28,6 +28,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    app.post('/users',(req, res)=>{
+        const newUser = req.body;
+        console.log(newUser)
+    })
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
